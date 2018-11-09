4939f4c5f3ef6aa54273f129960bcd9b0d92392c
hydro <michael.j.benton@live.com>
2018-11-08 17:17:13 -0800

    Revert "Lab 1 update 2.0 Finished"
    This reverts commit 705ed4c274abe525d4470a4646137193284379e6.

--- app/src/main/java/com/hydroliquid/mjben/flashapp/MainActivity.java
+++ app/src/main/java/com/hydroliquid/mjben/flashapp/MainActivity.java
@@ -4,24 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
+public class MainActivity extends AppCompatActivity
 {
-    // Visibility Variables
     boolean isShowingAnswers = false;
     boolean answerOn = false;
-    boolean firstQuestion = false;
-    boolean secondQuestion = false;
-    boolean greenOn = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
         // Quick Access
         TextView flashQuest = findViewById(R.id.flashcard_question);
         TextView flashAnswer = findViewById(R.id.flashcard_anwser);
@@ -481,3 +476,137 @@ findViewById(R.id.flashcard_answerCorrect3).setOnClickListener(new View.OnClickL
 });
 */
 // End Answer 3
+=======
+
+        ((ImageView) findViewById(R.id.toggle_choices_visibility)).setOnClickListener
+                (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isShowingAnswers) {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource
+                            (R.drawable.ic_check_mark);
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer3).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = true;
+                }
+                else {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource
+                            (R.drawable.ic_question_mark_circle);
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+                    isShowingAnswers = false;
+                }
+            }
+        });
+        
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!answerOn) {
+                    findViewById(R.id.flashcard_anwser).setVisibility(View.INVISIBLE);
+
+                    findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+
+                    answerOn = true;
+                }
+            }
+        });
+        findViewById(R.id.flashcard_answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!answerOn) {
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer3).setVisibility(View.INVISIBLE);
+
+                    findViewById(R.id.flashcard_answerWrong1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answerCorrect3).setVisibility(View.VISIBLE);
+
+                    answerOn = true;
+                }
+            }
+        });
+        findViewById(R.id.flashcard_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!answerOn) {
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer3).setVisibility(View.INVISIBLE);
+
+                    findViewById(R.id.flashcard_answerWrong2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answerCorrect3).setVisibility(View.VISIBLE);
+                    answerOn = true;
+                }
+            }
+        });
+        findViewById(R.id.flashcard_answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!answerOn) {
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answerCorrect3).setVisibility(View.VISIBLE);
+                    answerOn = true;
+                }
+
+            }
+        });
+
+        findViewById(R.id.flashcard_anwser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(answerOn) {
+                    findViewById(R.id.flashcard_anwser).setVisibility(View.VISIBLE);
+
+                    findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+
+                    answerOn = false;
+                }
+            }
+        });
+        findViewById(R.id.flashcard_answerWrong1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(answerOn) {
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+
+                    findViewById(R.id.flashcard_answerWrong1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answerWrong2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answerCorrect3).setVisibility(View.INVISIBLE);
+
+                    answerOn = false;
+                }
+            }
+        });
+
+        findViewById(R.id.flashcard_answerWrong2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(answerOn) {
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+
+                    findViewById(R.id.flashcard_answerWrong1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answerWrong2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answerCorrect3).setVisibility(View.INVISIBLE);
+                    answerOn = false;
+                }
+            }
+        });
+
+        findViewById(R.id.flashcard_answerCorrect3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(!answerOn){
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answerCorrect3).setVisibility(View.INVISIBLE);
+                    answerOn = false;
+                }
+            }
+        });
+    }
+}
+>>>>>>> parent of 705ed4c... Lab 1 update 2.0 Finished
